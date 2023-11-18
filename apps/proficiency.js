5f651ab83173ec645ee6b52914c4401b6e1455df
micuks <sdkjwql@gmail.com>
2023-11-18 12:01:08 +0800

    feat: proficiency query framework

---
+++ apps/proficiency.js
@@ -0,0 +1,22 @@
+/**
+ * 干货助手API
+ */
+
+import { ProficiencyQuery } from "./proficiency/ProficiencyQuery.js";
+import { App } from "#honor";
+
+let app = App.init({
+  id: "proficiency",
+  name: "战力查询",
+});
+
+app.reg({
+  proficiencyQuery: {
+    name: "战力查询",
+    rule: /^#*([^#]+)\s*(honor)?(查战力|战力|查询|战斗力|战力查询|战斗力查询|战斗力查询)?(安卓|安|果|苹果|ios|android|iOS|Android|华为|huawei|Huawei|HUAWEI)?(QQ|q|qq|微信|w|wx|WX)?$/,
+    fn: proficiencyQuery,
+    desc: "[#战力查询] 查询战力",
+  },
+});
+
+export default app;
