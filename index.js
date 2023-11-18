13271720f176b02beaa51949a6bb40056bafe3e2
micuks <sdkjwql@gmail.com>
2023-11-18 10:48:37 +0800

    feat: index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+if (!global.segment) {
+    global.segment = (await import('oicq')).segment
+}
+
+export * from './apps/index.js'
+
+if (Bot?.logger?.info) {
+    Bot.logger.info('------O.o------')
+    Bot.logger.info(`农活插件${Version.version}初始化~`)
+} else {
+    console.log(`农活插件${Version.version}初始化~`)
+}
