84d98664af2f7f1fa87591e606e206e82507391f
micuks <sdkjwql@gmail.com>
2023-11-18 21:02:11 +0800

    feat: Init tool

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+import { Data, Version } from "#honor";
+import Index from "./tools/index.js";
+
 if (!global.segment) {
-    global.segment = (await import('oicq')).segment
+  global.segment = (await import("oicq")).segment;
 }
 
-export * from './apps/index.js'
+export * from "./apps/index.js";
 
 if (Bot?.logger?.info) {
-    Bot.logger.info('------O.o------')
-    Bot.logger.info(`农活插件${Version.version}初始化~`)
+  Bot.logger.info("------O.o------");
+  Bot.logger.info(`农活插件${Version.version}初始化~`);
 } else {
-    console.log(`农活插件${Version.version}初始化~`)
+  console.log(`农活插件${Version.version}初始化~`);
 }
+
+setTimeout(Index.init, 1000);
---
+++ tools/index.js
@@ -0,0 +1,48 @@
+import { Data, Version } from "#honor";
+import fs, { readdirSync } from "node:fs";
+import fs from "node:fs";
+
+let replyPrivate = async function () {};
+let common = await Data.importModule("lib/common/common.js", "root");
+if (common && common.default && common.default.replyPrivate) {
+  replyPrivate = common.default.replyPrivate;
+}
+
+const Index = {
+  async init() {
+    await Index.checkVersion();
+    await Index.startMsg();
+  },
+
+  async startMsg() {
+    let mstStr = await redis.get("honor:restart-msg");
+    if (msgStr) {
+      let msg = JSON.parse(msgStr);
+      await replyPrivate(msg.qq, msg.msg);
+      await redis.del("honor:restart-msg");
+      let msgs = [
+        `Current Honor plugin version: ${Version.version}`,
+        "You can use #honor版本 command to check changelog",
+      ];
+      await replyPrivate(msg.qq, msgs.join("\n"));
+    }
+  },
+
+  async checkVersion() {
+    if (!Version.isV3) {
+      console.log(
+        `ERROR: honor-plugin require V3 Yunzai to function properly.`
+      );
+    }
+    if (!fs.existsSync(process.cwd() + "/lib/plugins/runtime.js")) {
+      let msg =
+        "ERROR: Runtime file not found, please check if you have installed Yunzai-Bot V3 or Miao-Yunzai correctly.";
+      if (!(await redis.get("honor:runtime-warning"))) {
+        await replyPrivate(msg.qq, msg);
+        await redis.set("honor-runtime-warning", "true", { EX: 3600 * 24 });
+      } else {
+        console.log(msg);
+      }
+    }
+  },
+};
