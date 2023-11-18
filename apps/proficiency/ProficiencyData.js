73e5e11372637feb71d1ff8487582a97da5741d0
micuks <sdkjwql@gmail.com>
2023-11-18 17:03:41 +0800

    feat: Hero Proficiency Class

--- apps/proficiency/ProficiencyData.js
+++ apps/proficiency/ProficiencyData.js
@@ -4,6 +4,9 @@ import { Format } from "#honor";
 
 let ProficiencyData = {
   async getProficiency(hero_name, platform) {
-    let proficiencyData = await GanHuoApi.getProficiency();
+    let proficiencyData =
+      (await GanHuoApi.getHeroProficiency(hero_name, platform)) || {};
+    proficiencyData = proficiencyData.data || {};
+      let {uid, name, alias, platform, photo, area, }
   },
 };
---
+++ apps/proficiency/heroData.js
@@ -0,0 +1,33 @@
+class HeroProficiency {
+  constructor(
+    uid,
+    name,
+    alias,
+    platform,
+    photo,
+    area,
+    areaPower,
+    city,
+    cityPower,
+    province,
+    provincePower,
+    guobiao,
+    stamp,
+    updatetime
+  ) {
+    this.uid = uid;
+    this.name = name;
+    this.alias = alias;
+    this.platform = platform;
+    this.photo = photo;
+    this.area = area;
+    this.areaPower = areaPower;
+    this.city = city;
+    this.cityPower = cityPower;
+    this.province = province;
+    this.provincePower = provincePower;
+    this.guobiao = guobiao;
+    this.stamp = stamp;
+    this.updatetime = updatetime;
+  }
+}
