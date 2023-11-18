ed2e31454f08b2b890f53a9537c5996cd893ad5a
micuks <sdkjwql@gmail.com>
2023-11-18 20:51:02 +0800

    feat: vanilla query feature

---
+++ apps/help/Help.js
@@ -0,0 +1,11 @@
+const Help = {
+  // TODO: Enrich me
+  render: function () {
+    return `#Honor帮助 #Honor版本`;
+  },
+  version: function () {
+    return `Honor版本`;
+  },
+};
+
+export default Help;
--- apps/proficiency/ProficiencyQuery.js
+++ apps/proficiency/ProficiencyQuery.js
@@ -3,7 +3,28 @@ import ProficiencyData from "./ProficiencyData";
 import { Common } from "#honor";
 
 const ProficiencyQuery = {
-  async query(e) {},
+  async query(e) {
+    let hero_name = e.matches[1];
+    let platform = e.matches[4] || "安卓";
+    let proficiency = await ProficiencyData.getProficiency(hero_name, platform);
+    if (lodash.isEmpty(proficiency)) {
+      e.reply("没有找到该英雄的战力信息");
+      return;
+    }
+    let msg = Common.format(
+      "{0}({1})\n战力: {2}\n区服: {3}\n城市: {4}\n省份: {5}\n国标: {6}\n更新时间: {7}",
+      proficiency.name,
+      proficiency.alias,
+      proficiency.power,
+      proficiency.area,
+      proficiency.city,
+      proficiency.province,
+      proficiency.guobiao,
+      proficiency.updatetime
+    );
+    e.reply(msg);
+    return true;
+  },
 };
 
 export default ProficiencyQuery;
