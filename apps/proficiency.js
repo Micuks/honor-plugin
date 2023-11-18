9d0579ac1e4428ba8af2ce9c07f13ef128e03f41
micuks <sdkjwql@gmail.com>
2023-11-18 21:34:59 +0800

    fixup: fix wrong capturing

--- apps/proficiency.js
+++ apps/proficiency.js
@@ -13,7 +13,7 @@ let app = App.init({
 app.reg({
   proficiencyQuery: {
     name: "战力查询",
-    rule: /^#*([^#]+)\s*(honor)?(查战力|战力|查询|战斗力|战力查询|战斗力查询|战斗力查询)?(安卓|安|果|苹果|ios|android|iOS|Android|华为|huawei|Huawei|HUAWEI)?(QQ|q|qq|微信|w|wx|WX)?$/,
+    rule: /^#*([^#]+?)\s*(?:honor)?(?:查战力|战力|查询|战斗力|战力查询|战斗力查询|战斗力查询)(安卓|安|果|苹果|ios|android|iOS|Android|华为|huawei|Huawei|HUAWEI)?(QQ|q|qq|微信|w|wx|WX)?$/,
     fn: ProficiencyQuery.query,
     desc: "[#战力查询] 查询战力",
   },
