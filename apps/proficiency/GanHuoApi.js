3c58b93a1ff01cd7b93267a8de4a9ccea51f8d05
micuks <sdkjwql@gmail.com>
2023-11-18 12:02:25 +0800

    feat: Proficiency query framework

---
+++ apps/proficiency/GanHuoApi.js
@@ -0,0 +1,11 @@
+/**
+ * 干货助手API Honor-Plugin 封装
+ * https://www.sapi.run/wangzhe/
+ */
+
+import fetch from "node-fetch";
+import { Data } from "#honor";
+
+const host = "https://www.sapi.run/hero/select.php";
+
+function getApi(api) {}
