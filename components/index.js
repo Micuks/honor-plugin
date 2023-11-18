e60ab2b8f3eaa9e80a6f46d58dd4f7109ba0b754
micuks <sdkjwql@gmail.com>
2023-11-18 21:09:45 +0800

    feat: export Data, Version, App, Format

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 import Data from "./Data.js";
 import Version from "./Version.js";
+import App from "./App.js";
+import Format from "./Format.js";
 
-export { Data, Version };
+export { Data, Version, App, Format };
