9d1362d0e070e5d387f1b63e852c61c3c1b8ff61
micuks <sdkjwql@gmail.com>
2023-11-18 21:13:08 +0800

    feat: export Common

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Data from "./Data.js";
 import Version from "./Version.js";
 import App from "./App.js";
 import Format from "./Format.js";
+import Common from "./Common.js";
 
-export { Data, Version, App, Format };
+export { Common, Data, Version, App, Format };
