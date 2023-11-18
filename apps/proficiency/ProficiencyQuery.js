78ef397a2d9736b83f20606eb50747b421a926a9
micuks <sdkjwql@gmail.com>
2023-11-18 21:50:14 +0800

    feat: regularize query message

--- apps/proficiency/ProficiencyQuery.js
+++ apps/proficiency/ProficiencyQuery.js
@@ -2,13 +2,47 @@ import lodash from "lodash";
 import ProficiencyData from "./ProficiencyData.js";
 import { Common } from "#honor";
 
+const proficiencyReg =
+  /^#*([^#]+?)\s*(?:honor)?(?:查战力|战力|查询|战斗力|战力查询|战斗力查询|战斗力查询)(安卓|安|果|苹果|ios|android|iOS|Android|华为|huawei|Huawei|HUAWEI)?(QQ|q|qq|微信|w|wx|WX)?$/;
+
 const ProficiencyQuery = {
   async query(e) {
-    console.log(e.matches);
-    let hero_name = e.matches[1];
-    let platform = e.matches[4] || "安卓";
-    console.log(hero_name, platform);
-    let proficiency = await ProficiencyData.getProficiency(hero_name, platform);
+    let ret = proficiencyReg.exec(msg);
+
+    console.log(ret);
+    // TODO: Platform regularize to android and ios
+    if (length(ret) === 4) {
+      e.hero_name = ret[1];
+      e.os = ret[2] || "android";
+      e.platform = ret[3] || "qq";
+    } else if (length(ret) === 2) {
+      e.hero_name = ret[1];
+      e.os = ret[2] || "android";
+      e.platform = "qq";
+    }
+
+    // regularize os
+    if (e.os in ["安卓", "安", "android", "Android"]) {
+      e.os = "a";
+    } else {
+      e.os = "i";
+    }
+
+    // regularize platform
+    if (e.platform in ["QQ", "qq", "q"]) {
+      e.platform = "qq";
+    } else {
+      e.platform = "wx";
+    }
+
+    e.platform = `${e.os}${e.platform}}`;
+
+    console.log(e.hero_name, e.platform);
+
+    let proficiency = await ProficiencyData.getProficiency(
+      e.hero_name,
+      e.platform
+    );
     if (lodash.isEmpty(proficiency)) {
       e.reply("没有找到该英雄的战力信息");
       return;
