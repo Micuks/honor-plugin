4207ddc21c93abf6cb9b8ca807968a7b3759637b
micuks <sdkjwql@gmail.com>
2023-11-19 23:21:23 +0800

    feat: HeroInfo render

--- apps/statistics/HeroInfo.js
+++ apps/statistics/HeroInfo.js
@@ -1,5 +1,7 @@
-import lodash from "lodash";
+import _ from "lodash";
+import { getHeroInfoRefresh } from "./HeroCommon";
 import { Common, Data, Format } from "#honor";
+import { Hero } from "#honor.models";
 
 const heroInfoReg = /^#*([^#]+)\s*(查询|详细|详情|数据|信息)\s*$/;
 
@@ -12,12 +14,32 @@ let HeroInfo = {
 
     let ret = heroInfoReg.exec(msg);
 
-    let heroName = Hero.get();
+    let heroName = ret[1];
+    let hero = Hero.get(heroName);
+    if (!hero) {
+      return false;
+    }
 
-    return HeroInfo.render(e, heroName);
+    return HeroInfo.render({ e, hero });
   },
 
-  async render(e, heroName, params = {}) {},
+  async render({ e, hero }) {
+    let heroInfo = e._heroInfo || (await getHeroInfoRefresh(e, hero.id));
+    if (!heroInfo) {
+      return true;
+    }
+
+    // TODO: Implement
+
+    return await Common.render(
+      "hero-info/hero-info",
+      {
+        data,
+        imgs: hero.getImgs(),
+      },
+      { e, scale: 1.4 }
+    );
+  },
 };
 
 export default HeroInfo;
